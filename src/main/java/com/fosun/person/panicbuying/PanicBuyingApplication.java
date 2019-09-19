7d1a65794f04a772f1f27aeff22e8b90b1ec413a
liumch <liumch@fosun.com>
2019-09-19 16:55:08 +0800

    1、添加主从配置和实现 2、添加AspectJ切面处理

--- src/main/java/com/fosun/person/panicbuying/PanicBuyingApplication.java
+++ src/main/java/com/fosun/person/panicbuying/PanicBuyingApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * 注意，此处必须要将springboot的jdbc DataSource自动配置功能关闭，否则会报冲突
+ */
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 public class PanicBuyingApplication {
